3ebdb7e80a19f1b9015f59ac1089e9d540f88639
luigi-ichi <159853434+luigi-ichi@users.noreply.github.com>
2025-10-05 08:19:24 +0800

    Initial commit

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,22 @@
+class PasswordsController < ApplicationController
+  before_action :authenticate_user!
+
+  def edit
+    # Render password form here
+  end
+
+  def update
+    if current_user.update(password_params)
+      redirect_to edit_password_path, notice: "Password updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def password_params
+    params.expect(user: [:password, :password_confirmation, :password_challenge]).with_defaults(password_challenge: "")
+    # params.require(:user).permit(:password, :password_confirmation, :password_challenge).with_defaults(password_challenge: "")
+  end
+end
