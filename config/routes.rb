743ff0f3e4275af2a443dd0992d88677de31611f
luigi-ichi <159853434+luigi-ichi@users.noreply.github.com>
2025-10-10 23:12:06 +0800

    fix: routing to the root of /books/ no longer penetratable, which may cause a lot of bugs and unexpected behavior

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :books, only: [:index, :show, :edit, :update, :destroy]
+  # resources :books, only: [:index, :show, :edit, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resource :registration, only: [ :new, :create ]
   resource :session, only: [ :new, :create, :destroy ]
