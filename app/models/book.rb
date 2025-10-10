65615c977334fd2a7fcdc2634908f67784c9fb4a
luigi-ichi <159853434+luigi-ichi@users.noreply.github.com>
2025-10-10 22:48:34 +0800

    feat: added validation for unique titles and number ranges in chapters and volumes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :list
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: { scope: :list_id }
+  validates :chaptersRead, numericality: { greater_than_or_equal_to: 0 }
+  validates :volumesRead, numericality: { greater_than_or_equal_to: 0 }
 
   def has_half_chapter?
     (self.chaptersRead % 1) == 0.5
