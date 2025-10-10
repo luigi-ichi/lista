5c7adca579b33a252269dbb4974e72cd91c2e981
luigi-ichi <159853434+luigi-ichi@users.noreply.github.com>
2025-10-10 19:33:36 +0800

    feat: add title validation and increment chapter function

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,19 @@
 class Book < ApplicationRecord
   belongs_to :list
+
+  validates :title, presence: true
+
+  def has_half_chapter?
+    (self.chaptersRead % 1) == 0.5
+  end
+
+  def increment_chapter
+    if has_half_chapter?
+      self.chaptersRead += 0.5
+    else
+      self.chaptersRead += 1
+    end
+    self.updated_at = Time.current
+    save
+  end
 end
